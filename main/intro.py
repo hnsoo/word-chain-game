d04af5b0dc69659864d843f2b767ae25a210737c
hnsoo <khsoo2439@gmail.com>
2022-05-29 01:20:17 +0900

    메인 서버 개발
    메인 서버를 소켓 통신을 이용하여 구현함.

--- main/intro.py
+++ main/intro.py
@@ -1,39 +1,32 @@
 from tkinter import *
 from tkinter import messagebox
+import socket
+import placeholder
 
+class Intro:
+    client_socket = None
 
-class EntryWithPlaceholder(Entry):
-    def __init__(self, master=None, placeholder="PLACEHOLDER", color='grey'):
-        super().__init__(master)
-
-        self.config(width=43, bd=2)
-        self.placeholder = placeholder
-        self.placeholder_color = color
-        self.default_fg_color = self['fg']
-
-        self.bind("<FocusIn>", self.foc_in)
-        self.bind("<FocusOut>", self.foc_out)
-
-        self.put_placeholder()
-
-    def put_placeholder(self):
-        self.insert(0, self.placeholder)
-        self['fg'] = self.placeholder_color
-
-    def foc_in(self, *args):
-        if self['fg'] == self.placeholder_color:
-            self.delete('0', 'end')
-            self['fg'] = self.default_fg_color
-
-    def foc_out(self, *args):
-        if not self.get():
-            self.put_placeholder()
-
-
-class Intro():
-    def __init__(self):
+    def __init__(self, master):
         self.userName = ""
-        self.window = Tk()
+
+        # 클래스 속성
+        self.window = master
+        self.bg = None
+        self.bg_label = None
+        self.textbox = None
+        self.button = None
+        # 클래스 메소드
+        self.init_socket()
+        self.init_gui()
+
+    def init_socket(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        remote_ip = '127.0.0.1'
+        remote_port = 55555
+        self.client_socket.connect((remote_ip, remote_port))
+
+    def init_gui(self):
+        # 윈도우 창
         self.window.title("word chain game")
         self.window.geometry("1001x700+100+100")
         self.window.resizable(False, False)
@@ -41,17 +34,13 @@ class Intro():
         self.bg = PhotoImage(file='../img/intro-page.png')
         self.bg_label = Label(self.window, image=self.bg)
         self.bg_label.place(x=0, y=0)
-
         # 닉네임 입력 박스
-        self.textbox = textbox = EntryWithPlaceholder(self.window, "영문, 숫자 최대 10자리")
+        self.textbox = placeholder.EntryWithPlaceholder(self.window, "영문, 숫자 최대 10자리")
         self.textbox.place(x=350, y=300)
-
         # 시작 버튼
         self.button = Button(self.window, text="Go", bg="#ebddcc", fg="black", overrelief="solid", width=10,
                              repeatdelay=1000, repeatinterval=100, command=self.nameCheck)
         self.button.place(x=460, y=350)
-
-        mainloop()
 
     def nameCheck(self):
         count = 0
@@ -63,11 +52,24 @@ class Intro():
             if count != len(self.textbox.get()):
                 messagebox.showwarning("닉네임 양식 오류", "영문, 숫자 최대 10자리로 닉네임을 생성해주세요.")
             else:
-                self.userName = self.textbox.get()
-                self.window.destroy()
+                self.client_socket.send(self.textbox.get().encode('utf-8'))
+                if self.receive_response():
+                    self.userName = self.textbox.get()
+                    self.window.destroy()
+                else:
+                    messagebox.showwarning("닉네임 중복 오류", "서버 상에 해당 닉네임이 존재합니다. ")
+                    self.client_socket.close()
+                    self.init_socket()
         else:
             messagebox.showwarning("닉네임 양식 오류", "영문, 숫자 최대 10자리로 닉네임을 생성해주세요.")
 
+    def receive_response(self):
+        while True:
+            buffer = self.client_socket.recv(256).decode('utf-8')
+            if not buffer or "no" in buffer:
+                return False
+            else:
+                return True
+
     def retName(self):
         return self.userName
-
--- main/main.py
+++ main/main.py
@@ -1,9 +1,14 @@
 from tkinter import Tk, mainloop
 
-import intro,room
+import intro, room
+import ingame
 
 if __name__ == "__main__":
-    p = intro.Intro()
+    # 게임 처음 화면
+    window = Tk()
+    p = intro.Intro(window)
+    window.mainloop()
+
     print(p.retName())
     p = room.Room()
 
