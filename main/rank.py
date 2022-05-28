4f1db3dadbeae329fe120479d5f3f6676635c280
DESKTOP-53UP5HU\ymzkx <zmfflr156@naver.com>
2022-05-28 22:39:09 +0900

    feat: 코드 수정
    기존에 있던 코드들을 객체화 시킨 코드로 변경

--- main/rank.py
+++ main/rank.py
@@ -1,27 +1,22 @@
 from tkinter import *
 
-window = Tk()
+class Rank():
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Ranking")
+        self.window.geometry("1001x700+100+100")
+        self.window.resizable(False, False)
 
-window.title("Ranking")
-window.geometry("1001x700+100+100")
-window.resizable(False, False)
+        self.bg = PhotoImage(file="../img/ranking-page.png")
+        self.bg_label = Label(self.window, image=self.bg)
+        self.bg_label.place(x=0, y=0)
 
-bg = PhotoImage(file="../img/ranking-page.png")
-bg_label = Label(window, image=bg)
-bg_label.place(x=0, y=0)
+        self.btn_start = Button(self.window, text='계속하기', font=("Homenaje", 20, "bold"), anchor='center', width=18, height=3, bg='#72F987') #command="~~~" 시켜서 재시작 화면으로 넘겨야함
+        self.btn_start.place(x=170, y=330)
+        self.btn_exit = Button(self.window, text='종료하기', font=("Homenaje", 20, "bold"), anchor='center', width=18, height=3, bg='#A63641') #command="~~~"  재시작 화면으로 넘겨야함
+        self.btn_exit.place(x=550, y=330)
 
-
-btn_start = Button(window, text='계속하기', font=("Homenaje", 20, "bold"), anchor='center', width=18, height=3, bg='#72F987') #command="~~~" 시켜서 재시작 화면으로 넘겨야함
-btn_start.place(x=170, y=330)
-
-btn_exit = Button(window, text='종료하기', font=("Homenaje", 20, "bold"), anchor='center', width=18, height=3, bg='#A63641') #command="~~~" 시켜서 재시작 화면으로 넘겨야함
-btn_exit.place(x=550, y=330)
-
-
-
-
-
-mainloop()
+        mainloop()
 
 
 
