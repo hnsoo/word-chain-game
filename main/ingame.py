c5650d63007555b79d4df38d9b79320df62ad14f
DESKTOP-53UP5HU\ymzkx <zmfflr156@naver.com>
2022-05-30 00:27:11 +0900

    feat: 코드 추가
    나가기 버튼 구현을 위한 코드 추가 및 창 전환을 취한 코드 객체화

--- main/ingame.py
+++ main/ingame.py
@@ -1,9 +1,13 @@
 from tkinter import *
 
-class Ingame:
-    def __init__(self):
-        self.window = Tk()
 
+class Ingame:
+    def __init__(self, master):
+        self.window = master
+        self.init_gui()
+
+    def init_gui(self):
+        # 윈도우 창
         self.window.title("word chain game")
         self.window.geometry("1001x700+100+100")
         self.window.resizable(False, False)
@@ -13,4 +17,7 @@ class Ingame:
         self.bg_label = Label(self.window, image=self.bg)
         self.bg_label.place(x=0, y=0)
 
-        mainloop()
+        # 나가기
+        self.exit = None
+        self.exit = Button(self.window, text='나가기', anchor='center', width=20, height=3, bg='grey', fg='white')
+        self.exit.place(x=0, y=0)
