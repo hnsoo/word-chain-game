3592e1faade14226c9f62df45c2a979a274f9be0
hnsoo <khsoo2439@gmail.com>
2022-06-03 21:57:50 +0900

    소켓 생성을 메인으로 변경

--- main/main.py
+++ main/main.py
@@ -1,9 +1,14 @@
 from tkinter import Tk, mainloop
 
+import socket
 import intro, room
 import ingame
 
+
 if __name__ == "__main__":
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('127.0.0.1', 55555))
+
     # 게임 처음 화면
     window = Tk()
     p = intro.Intro(window)
@@ -13,6 +18,3 @@ if __name__ == "__main__":
     window = Tk()
     p = room.Room(window)
     window.mainloop()
-
-
-
